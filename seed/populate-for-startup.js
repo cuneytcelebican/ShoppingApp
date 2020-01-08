8b648040b0eced70c2715daef132b899524e04f1
Cuneyt Celebican <cuneytcelebican@gmail.com>
2020-01-08 17:38:21 -0500

    updated seed

--- seed/populate-for-startup.js
+++ seed/populate-for-startup.js
@@ -147,7 +147,7 @@ function insertProducts(callback)
     [
         new Product({
             _id: "5bedf31cc14d7822b39d9d43",
-            imagePath: 'uploads/7568644802_1_1_1.jpg',
+            imagePath: '/uploads/7568644802_1_1_1.jpg',
             title: 'Oversized Textured Top',
             description: 'High collar top with short cuffed sleeves. Asymmetric hem with side slits.',
             price: 35.95,
@@ -159,7 +159,7 @@ function insertProducts(callback)
         }),
         new Product({
             _id: "5bedf3b9c14d7822b39d9d45",
-            imagePath: 'uploads/5644641800_2_5_1.jpg',
+            imagePath: '/uploads/5644641800_2_5_1.jpg',
             title: 'Tank Top',
             description: 'Flowy V-neck camisole with spaghetti straps.',
             price: 29.99,
@@ -171,7 +171,7 @@ function insertProducts(callback)
         }),
         new Product({
             _id: "5bedf448c14d7822b39d9d47",
-            imagePath: 'uploads/7568469251_2_1_1.jpg',
+            imagePath: '/uploads/7568469251_2_1_1.jpg',
             title: 'Basic Top',
             description: 'Round neck long sleeved shirt. ',
             price: 25.99,
@@ -183,7 +183,7 @@ function insertProducts(callback)
         }),
         new Product({
             _id: "5bedf55bc14d7822b39d9d4b",
-            imagePath: 'uploads/8197757093_2_2_1.jpg',
+            imagePath: '/uploads/8197757093_2_2_1.jpg',
             title: 'Belted Plaid Blazer',
             description: 'Flowy blazer with lapel collar and long sleeves. Self belt. Chest patch pockets and welt pockets at hip. Front double-breasted button closure.',
             price: 79.99,
@@ -195,7 +195,7 @@ function insertProducts(callback)
         }),
         new Product({
             _id: "5bedf5eec14d7822b39d9d4e",
-            imagePath: 'uploads/1775300615_1_1_1.jpg',
+            imagePath: '/uploads/1775300615_1_1_1.jpg',
             title: 'Perl Knit Swear',
             description: 'Purl-stitch knit sweater in a combination of textures. Ribbed trim.',
             price: 79.99,
@@ -207,7 +207,7 @@ function insertProducts(callback)
         }),
         new Product({
             _id: "5bedf6b5c14d7822b39d9d51",
-            imagePath: 'uploads/6186352407_2_1_1.jpg',
+            imagePath: '/uploads/6186352407_2_1_1.jpg',
             title: 'Ripped Jeans',
             description: 'Slim fit jeans with five pockets, washed effect, and rips on the legs. Zippered hem at in-seams. Front zip and metal button closure.',
             price: 79.99,
@@ -219,7 +219,7 @@ function insertProducts(callback)
         }),
         new Product({
             _id: "5bedf720c14d7822b39d9d52",
-            imagePath: 'uploads/5575380406_1_1_1.jpg',
+            imagePath: '/uploads/5575380406_1_1_1.jpg',
             title: 'Basic Slim Jeans',
             description: 'Basic slim-fit jeans with five pockets. Two side pockets, two back pockets, and one coin pocket. Belt loops. Front hidden zipper and button closure.',
             price: 45.99,
@@ -231,7 +231,7 @@ function insertProducts(callback)
         }),
         new Product({
             _id: "5bedf7ecc14d7822b39d9d55",
-            imagePath: 'uploads/3548350700_2_1_1.jpg',
+            imagePath: '/uploads/3548350700_2_1_1.jpg',
             title: 'Faux Leather Perforated Jacket',
             description: 'Faux leather perforated jacket with high collar and long sleeves. Two front zip pockets. Lined. Interior pocket. Front zip closure. Ribbed elastic hem and cuffs.',
             price: 99.99,
@@ -259,42 +259,42 @@ function insertVariants(callback)
     [
         new Variant({
             productID: '5bedf31cc14d7822b39d9d43',
-            imagePath: 'uploads/7568644710_1_1_1.jpg',
+            imagePath: '/uploads/7568644710_1_1_1.jpg',
             color: 'Beige',
             size: 'S,L',
             quantity: 5,
         }),
         new Variant({
             productID: '5bedf3b9c14d7822b39d9d45',
-            imagePath: 'uploads/5644641735_2_5_1.jpg',
+            imagePath: '/uploads/5644641735_2_5_1.jpg',
             color: 'Copper',
             size: 'S,L,XL',
             quantity: 12,
         }),
         new Variant({
             productID: '5bedf448c14d7822b39d9d47',
-            imagePath: 'uploads/7568469605_2_1_1.jpg',
+            imagePath: '/uploads/7568469605_2_1_1.jpg',
             color: 'Maroon',
             size: 'XS,M,L',
             quantity: 4,
         }),
         new Variant({
             productID: '5bedf448c14d7822b39d9d47',
-            imagePath: 'uploads/7568469822_2_1_1.jpg',
+            imagePath: '/uploads/7568469822_2_1_1.jpg',
             color: 'Charcool',
             size: 'XS,L,XL',
             quantity: 5,
         }),
         new Variant({
             productID: '5bedf5eec14d7822b39d9d4e',
-            imagePath: 'uploads/1775300806_2_1_1.jpg',
+            imagePath: '/uploads/1775300806_2_1_1.jpg',
             color: 'Stone',
             size: 'S,XL',
             quantity: 35,
         }),
         new Variant({
             productID: '5bedf720c14d7822b39d9d52',
-            imagePath: 'uploads/5575380407_1_1_1.jpg',
+            imagePath: '/uploads/5575380407_1_1_1.jpg',
             color: 'Dark Blue',
             size: 'M,XL',
             quantity: 5,
